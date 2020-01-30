3a031a8f0ee30a203e86a3dab1f484ef9b670aae
Abel <kisebela.a@gmail.com>
2020-01-30 10:23:49 +0100

    Ajour de nouveau fichier docker

---
+++ Final-Scripts/Docker/demmarer_service-docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+if [ -e rm /var/run/docker.pid ]
+then
+	rm /var/run/docker.pid
+else
+	echo "Pas besoin de supprimer docker.pid next "
+fi
+dockerd
+docker-compose -f docker-compose-test.yml up
